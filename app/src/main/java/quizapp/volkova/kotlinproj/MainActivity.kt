bb52290ec06992e6f62553e16c99ab5d9f0f7741
Your Name <liza.volk999@gmail.com>
2021-09-10 13:15:12 +0300

    connection between seekbar and textview

--- app/src/main/java/quizapp/volkova/kotlinproj/MainActivity.kt
+++ app/src/main/java/quizapp/volkova/kotlinproj/MainActivity.kt
@@ -2,6 +2,7 @@ package quizapp.volkova.kotlinproj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,32 @@ class MainActivity : AppCompatActivity() {
 
         val initialTextViewTranslationY = textView_progress.translationY
 
-        
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textView_progress.text = progress.toString()
+
+                val translationDistance = (initialTextViewTranslationY + progress * resources.getDimension(R.dimen.text_anim_step) * -1)
+
+                textView_progress.animate().translationY(translationDistance)
+
+                if(!fromUser)
+                    textView_progress.animate().setDuration(500).rotationBy(360f)
+                        .translationY(initialTextViewTranslationY)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+        reset_button.setOnClickListener { v ->
+            seekBar.progress = 0
+
+        }
     }
 }
